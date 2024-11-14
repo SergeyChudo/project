77c45cfffbf79b4615e56da0967d5a9654ee4f0f
SergeyChudo <mrvidalta.43@gmail.com>
2024-11-14 10:55:44 +0300

    first project

---
+++ main.js
@@ -0,0 +1,22 @@
+let a = `10`;
+const b = 15;
+
+
+console.log(typeof a)
+console.log(typeof b)
+ console.log(a+b)
+ console.log(b-a)
+ console.log(b*a)
+ console.log(a/b)
+ console.log(a**b)
+
+ alert(`Привет`)
+ confirm(`Ты точно хочешь выйти?`)
+ prompt(`Сколько тебе лет?`)
+let result = prompt(`Сколько тебе лет?`)
+console.log(result)
+
+
+
+
+ 
